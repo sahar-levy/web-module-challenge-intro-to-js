0a02512206e9686e12b105ccd92de1a838bb6a2f
Sahar Levy <sahar@nogalesfundmgmt.com>
2023-02-02 13:48:12 -0800

    Task 3 hungryDog function and conditionals practice

--- index.js
+++ index.js
@@ -142,10 +142,27 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age){
+  if (age >= 1 && weight <= 5) {
+    return weight * 0.05;
+  } else if (age >= 1 && weight >= 6 && weight <= 10) {
+    return weight * 0.04;
+  } else if (age >= 1 && weight >= 11 && weight <= 15){
+    return weight * 0.03;
+  } else if (age >= 1 && weight > 15) {
+    return weight * 0.02;
+  } else if (age > 1 && age >= 0.583) {
+    return weight * 0.04;
+  } else if (age < 0.583 && age >= 0.333) {
+    return age * 0.05;
+  } else if (age < 0.333) {
+    return age * 0.10;
+  } else {
+    return 'Please try again.';
+  }
 }
 
+console.log('Task 3:', hungryDog(15, 1));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
